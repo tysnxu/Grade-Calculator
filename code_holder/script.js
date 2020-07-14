fc2431c7a175e7b7000a2218a70b303be0737564
tysnxu <52490024+tysnxu@users.noreply.github.com>
2020-07-14 18:28:46 +0800

    Add files via upload

---
+++ code_holder/script.js
@@ -0,0 +1,24 @@
+function showHide(block_number) {
+    current_status = document.getElementsByClassName("textbox")[block_number].getAttribute("style")
+    if (current_status != "display: none") {
+        document.getElementsByClassName("textbox")[block_number].setAttribute("style", "display: none")
+    } else {
+    document.getElementsByClassName("textbox")[block_number].setAttribute("style", "display: block")
+    }
+}
+
+function copyText(block_number) {
+    var el = document.createElement('textarea');
+    el.value = document.getElementsByClassName("textbox")[block_number].innerText;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+
+    document.getElementsByClassName("copy_button")[block_number].innerText = "Copied"
+    document.getElementsByClassName("copy_button")[block_number].setAttribute("style", "background: #1ce65f; color: white")
+    setTimeout(function(){
+        document.getElementsByClassName("copy_button")[block_number].innerText = "Copy Code"
+        document.getElementsByClassName("copy_button")[block_number].removeAttribute("style")
+    }, 3000); 
+}
